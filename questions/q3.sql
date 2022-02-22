5d552dc93527ffa10e8c9237fa65be340298f6b3
conner-mcnicholas <connermcnicholas@gmail.com>
2022-02-21 20:37:07 -0800

    Changed q3 from IN to LEFT JOIN

--- questions/q3.sql
+++ questions/q3.sql
@@ -12,17 +12,32 @@ EXPLAIN ANALYZE SELECT name FROM Student WHERE id IN (SELECT studId FROM Transcr
 CREATE INDEX crsind ON Transcript (crsCode);
 ALTER TABLE Student ADD PRIMARY KEY(id);
 
----> Nested loop inner join  (cost=1.22 rows=2) (actual time=0.057..0.064 rows=2 loops=1)
---    -> Filter: (`<subquery2>`.studId is not null)  (cost=0.65..0.40 rows=2) (actual time=0.044..0.045 rows=2 loops=1)
---        -> Table scan on <subquery2>  (cost=1.26..2.52 rows=2) (actual time=0.001..0.001 rows=2 loops=1)
---            -> Materialize with deduplication  (cost=2.16..3.42 rows=2) (actual time=0.044..0.044 rows=2 loops=1)
---                -> Filter: (Transcript.studId is not null)  (cost=0.70 rows=2) (actual time=0.025..0.034 rows=2 loops=1)
---                    -> Index lookup on Transcript using crsind (crsCode=(@v4))  (cost=0.70 rows=2) (actual time=0.024..0.033 rows=2 loops=1)
---    -> Single-row index lookup on Student using PRIMARY (id=`<subquery2>`.studId)  (cost=0.72 rows=1) (actual time=0.008..0.008 rows=1 loops=2)
+EXPLAIN ANALYZE SELECT name FROM Student WHERE id IN (SELECT studId FROM Transcript WHERE crsCode = @v4);
+/*
+-> Nested loop inner join  (cost=1.22 rows=2) (actual time=0.096..0.108 rows=2 loops=1)
+    -> Filter: (`<subquery2>`.studId is not null)  (cost=0.65..0.40 rows=2) (actual time=0.073..0.075 rows=2 loops=1)
+        -> Table scan on <subquery2>  (cost=1.26..2.52 rows=2) (actual time=0.002..0.002 rows=2 loops=1)
+            -> Materialize with deduplication  (cost=2.16..3.42 rows=2) (actual time=0.072..0.073 rows=2 loops=1)
+                -> Filter: (Transcript.studId is not null)  (cost=0.70 rows=2) (actual time=0.043..0.056 rows=2 loops=1)
+                    -> Index lookup on Transcript using crsind (crsCode=''MGT382'')  (cost=0.70 rows=2) (actual time=0.041..0.054 rows=2 loops=1)
+    -> Single-row index lookup on Student using PRIMARY (id=`<subquery2>`.studId)  (cost=0.72 rows=1) (actual time=0.015..0.015 rows=1 loops=2)
+*/
 
 --Total time decreased from 1.2 to .064 after putting index on Transcript.crsCode and adding PRIMARY KEY on Student.id
 
-SELECT name FROM Student WHERE id IN (SELECT studId FROM Transcript WHERE crsCode = @v4);
+EXPLAIN ANALYZE SELECT name from Student LEFT JOIN Transcript ON Student.id = Transcript.studId WHERE crsCode = @v4;
+
+/*
+-> Nested loop inner join  (cost=2.15 rows=2) (actual time=0.028..0.035 rows=2 loops=1)
+    -> Filter: (Transcript.studId is not null)  (cost=0.70 rows=2) (actual time=0.018..0.023 rows=2 loops=1)
+        -> Index lookup on Transcript using crsind (crsCode=''MGT382'')  (cost=0.70 rows=2) (actual time=0.017..0.022 rows=2 loops=1)
+    -> Single-row index lookup on Student using PRIMARY (id=Transcript.studId)  (cost=0.68 rows=1) (actual time=0.005..0.005 rows=1 loops=2)
+
+*/
+
+--Time descreased further to .035 after converting the first WHERE IN clause to a LEFT JOIN on the ids, thus removing a table scan step from query plan
+
+SELECT name from Student LEFT JOIN Transcript ON Student.id = Transcript.studId WHERE crsCode = @v4;
 /*
 +--------------+
 | name         |
